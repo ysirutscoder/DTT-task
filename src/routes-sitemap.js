10f03de12c28f476cdbc69e2271529afa877c25a
212725700 <alexei.strug@ge.com>
2020-07-31 23:18:31 +0200

    merge feature branch into master

--- src/routes-sitemap.js
+++ src/routes-sitemap.js
@@ -3,13 +3,13 @@ module.exports = [
     {path: '/main', name: 'main'},
     {path: '/random', name: 'random'},
     {
-        path: '/main/:filmId', name: 'details',
+        path:'/main/:filmId/:genre', name:'details',
         meta: {
             sitemap: {
                 slugs: [
-                    { filmId: 1},
-                    { filmId: 2},
-                    { filmId: 3}
+                    { filmId: 1, genre: 1},
+                    { filmId: 2, genre: 2},
+                    { filmId: 3, genre: 3}
                 ],
             }
         }
